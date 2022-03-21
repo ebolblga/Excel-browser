9aadae80d9d8f782dabe603adeeb5b8e67a1f6c4
ebolblga <kkochanovskiy@gmail.com>
2022-03-21 16:54:46 +0300

    Add project files.

---
+++ ФиЛП Лаб 2/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace ФиЛП_Лаб_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(69)))), ((int)(((byte)(69)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.dataGridView1.Location = new System.Drawing.Point(383, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(538, 421);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(795, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(12, 473);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose subject:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1. Найти страну с наибольшей площадью.",
+            "2. Найти все страны, на территории которых находится указанный горный хребет.",
+            "3. Найти все страны, у которых число национальностей превышает заданную величину." +
+                "",
+            "4. Найти все горные хребты, находящиесы на территории указанной страны.",
+            "5. Найти все страны, у которых численность населения меньше заданной величины."});
+            this.comboBox1.Location = new System.Drawing.Point(108, 470);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(681, 23);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(795, 470);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Find";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(558, 441);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 21);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Location = new System.Drawing.Point(108, 444);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(444, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Additional input:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Visible = false;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(69)))), ((int)(((byte)(69)))));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.dataGridView2.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(371, 421);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(47)))), ((int)(((byte)(47)))));
+            this.ClientSize = new System.Drawing.Size(933, 505);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ИДБ-19-03 Кочановский К. А. ФиЛП Лаб 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ ФиЛП Лаб 2/Form1.cs
@@ -0,0 +1,224 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ФиЛП_Лаб_2
+{
+    public partial class Form1 : Form
+    {
+        DataTableCollection tablesCollection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog()
+            { Filter = "Excel Workbook|*.xlsx|Excel 97-2003 Workbook|*.xls", ValidateNames = true })
+            {
+                dialog.InitialDirectory = @"C:\Users\kirill\Desktop\Учеба\Семестр 6\Функциональное програмирование\Лаб 2";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (var fileStream = File.Open(dialog.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(fileStream))
+                        {
+                            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable=(_)=>new ExcelDataTableConfiguration() { UseHeaderRow = true}
+                            });
+                            tablesCollection = result.Tables;
+                            dataGridView1.DataSource = tablesCollection[0];
+                            dataGridView2.DataSource = tablesCollection[0];
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(tablesCollection !=null)
+            {
+                DataTable table0 = tablesCollection[0];
+                DataTable table1 = tablesCollection[1];
+                DataTable table2 = tablesCollection[2];
+                DataTable table3 = tablesCollection[3];
+
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        dataGridView1.DataSource = null;
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.ColumnCount = 2;
+                        dataGridView1.Columns[0].Name = "Страна";
+                        dataGridView1.Columns[1].Name = "Площадь";
+
+                        double max = (double)table0.Rows[0][2];
+                        string name = (string)table0.Rows[0][1];
+                        for (int i = 1; i < table0.Rows.Count; ++i)
+                            if (max <= (double)table0.Rows[i][2])
+                            {
+                                max = (double)table0.Rows[i][2];
+                                name = (string)table0.Rows[i][1];
+                            }
+
+                        dataGridView1.Rows.Add(name, max);
+                        break;
+
+                    case 1:
+                        dataGridView1.DataSource = null;
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.ColumnCount = 1;
+                        dataGridView1.Columns[0].Name = "Страна";
+
+                        string value1 = textBox1.Text;
+
+                        for (int i = 0; i < table1.Rows.Count; ++i)
+                            if (value1 == table1.Rows[i][5].ToString())
+                            {
+                                int index = Convert.ToInt32(table1.Rows[i][0]);
+                                for (int j = 1; j < table0.Rows.Count; ++j)
+                                    if (index == Convert.ToInt32(table0.Rows[j][0]))
+                                        dataGridView1.Rows.Add(table0.Rows[j][1]);
+                            }
+                        break;
+
+                    case 2:
+                        dataGridView1.DataSource = null;
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.ColumnCount = 2;
+                        dataGridView1.Columns[0].Name = "Страна";
+                        dataGridView1.Columns[1].Name = "Кол-во национальностей";
+
+                        try
+                        {
+                            int value2 = Convert.ToInt32(textBox1.Text);
+                            int[] ncount = new int[table3.Rows.Count];
+
+                            for (int i = 0; i < table3.Rows.Count; ++i)
+                                if (table3.Rows[i][1].ToString() != "")
+                                    ncount[Convert.ToInt32(table3.Rows[i][0])]++;
+
+
+                            for (int i = 1; i < ncount.Length; ++i)
+                                if (ncount[i] > value2)
+                                    dataGridView1.Rows.Add(table0.Rows[i][1], ncount[i]);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Ввили не инт", "Беда");
+                        }
+                        break;
+
+                    case 3:
+                        dataGridView1.DataSource = null;
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.ColumnCount = 1;
+                        dataGridView1.Columns[0].Name = "Горные хребты";
+
+                        string value3 = textBox1.Text;
+                        int index3 = -1;
+
+                        for (int i = 1; i < table0.Rows.Count; ++i)
+                            if (table0.Rows[i][1].ToString() == value3)
+                                index3 = Convert.ToInt32(table0.Rows[i][0]);
+
+                        for (int i = 1; i < table1.Rows.Count; ++i)
+                            if (Convert.ToInt32(table1.Rows[i][0]) == index3)
+                                if (table1.Rows[i][5].ToString() != "")
+                                    dataGridView1.Rows.Add(table1.Rows[i][5]);
+
+                        break;
+
+                    case 4:
+                        dataGridView1.DataSource = null;
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.ColumnCount = 2;
+                        dataGridView1.Columns[0].Name = "Страна";
+                        dataGridView1.Columns[1].Name = "Население";
+
+                        try
+                        {
+                            int value4 = Convert.ToInt32(textBox1.Text);
+
+                            for (int i = 0; i < table2.Rows.Count; ++i)
+                                if ((double)table2.Rows[i][1] < value4)
+                                    dataGridView1.Rows.Add(table0.Rows[i][1], table2.Rows[i][1]);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Ввили не инт", "Беда");
+                        }
+                        break;
+
+                    default:
+                        MessageBox.Show("Опция не выбрана", "Беда");
+                        break;
+                }
+            }
+            else
+                MessageBox.Show("База данных не открыта", "Беда");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    textBox1.Enabled = false;
+                    textBox1.Visible = false;
+                    label2.Visible = false;
+                    break;
+
+                case 1:
+                    textBox1.Enabled = true;
+                    textBox1.Visible = true;
+                    label2.Visible = true;
+                    label2.Text = "Укажите название горного хребта:";
+                    break;
+
+                case 2:
+                    textBox1.Enabled = true;
+                    textBox1.Visible = true;
+                    label2.Visible = true;
+                    label2.Text = "Введите число национальностей:";
+                    break;
+
+                case 3:
+                    textBox1.Enabled = true;
+                    textBox1.Visible = true;
+                    label2.Visible = true;
+                    label2.Text = "Укажите страну:";
+                    break;
+
+                case 4:
+                    textBox1.Enabled = true;
+                    textBox1.Visible = true;
+                    label2.Visible = true;
+                    label2.Text = "Введите численность населения:";
+                    break;
+
+                default:
+                    textBox1.Enabled = false;
+                    textBox1.Visible = false;
+                    label2.Visible = false;
+                    break;
+            }
+        }
+    }
+}
